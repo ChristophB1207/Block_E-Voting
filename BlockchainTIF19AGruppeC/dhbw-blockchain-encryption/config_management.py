969f13a3f9ec03e94404d3d23e16df93a610e5ea
scfreiburg09 <simonschneider02@t-online.de>
2022-12-09 11:36:02 +0100

    Added setup script

--- BlockchainTIF19AGruppeC/dhbw-blockchain-encryption/config_management.py
+++ BlockchainTIF19AGruppeC/dhbw-blockchain-encryption/config_management.py
@@ -3,19 +3,36 @@ import datetime
 import dateutil.parser
 import dateutil.tz
 
+voter_count = 0
 options = []
+start_timestamp = ""
+start_time = datetime.datetime.now()
 end_timestamp = ""
 end_time = datetime.datetime.now()
 
 #Initialize by reading in the config information
 with open("./config/config.json") as config_file:
     config_data = json.load(config_file)
-    options = config_data["Partei"]
-    end_timestamp = config_data["Date"]
+    voter_count = config_data["voterCount"]
+    options = config_data["options"]
+    start_timestamp = config_data["start"]
+    start_time = dateutil.parser.isoparse(start_timestamp)
+    end_timestamp = config_data["end"]
     end_time = dateutil.parser.isoparse(end_timestamp)
+
+#Declare functions for accessing config variables
+def get_voter_count():
+    return voter_count
 
 def get_options():
     return options
+
+def has_election_started():
+    current_time = datetime.datetime.now(dateutil.tz.UTC)
+    if current_time > start_time:
+        return True
+    else:
+        return False
 
 def is_election_over():
     current_time = datetime.datetime.now(dateutil.tz.UTC)
---
+++ BlockchainTIF19AGruppeC/dhbw-blockchain-encryption/setup_election.py
@@ -0,0 +1,44 @@
+import random
+import mariadb
+
+from auth_service import save_admin_keys
+from encryption_service import generate_admin_private_key
+from config_management import get_voter_count
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def get_connection():
+    new_connection = mariadb.connect(
+                user="root",
+                password="Gu3487#2399!!+dj8v83bnd",
+                host="127.0.0.1",
+                port=3306,
+                database="auth_register")
+    return new_connection
+
+conn = get_connection()
+cur = conn.cursor()
+voter_count = get_voter_count()
+#Clear tables
+cur.execute("TRUNCATE TABLE voters")
+cur.execute("TRUNCATE TABLE directory_server3")
+conn.commit()
+#Generate admin key
+(admin_pub_key, admin_priv_key) = generate_admin_private_key()
+save_admin_keys(admin_pub_key, admin_priv_key)
+#Generate information for each voter
+voter_info = []
+for i in range(voter_count):
+    letter_part = random.choice(alphabet)
+    number_part = f"{random.randint(0, 999999999):09}"
+    personal_id = letter_part + number_part
+    
+    auth_id = random.randint(100000, 999999)
+    
+    voter_info.append((personal_id, 0, auth_id))
+
+sql_query = "INSERT INTO voters (PERSON_ID, HAS_VOTED, AUTH_ID) VALUES (?, ?, ?)"
+cur.executemany(sql_query, voter_info)
+conn.commit()
+conn.close()
+    
